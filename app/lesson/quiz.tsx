cdbb65515afa11c0ecf9eb82ae1d6c9a4362dc93
Adrien <ad.tranchand@gmail.com>
2024-11-11 18:21:47 +0100

    lesson footer creation + quiz update

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,8 +3,9 @@ import { useState } from "react";
 
 import { challengeOptions, challenges } from "@/db/schema";
 
-import { Challenge } from "./challenge";
 import { Header } from "./header";
+import { Footer } from "./footer";
+import { Challenge } from "./challenge";
 import { QuestionBubble } from "./question-bubble";
 
 type Props = {
@@ -35,8 +36,17 @@ export const Quiz = ({
         return uncompletedIndex === -1 ? 0 : uncompletedIndex;
     });
 
+    const [selectedOption, setSelectedOption] = useState<number>();
+    const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
     const challenge = challenges[activeIndex]; // challenge = current challenge
     const options = challenge?.challengeOptions ?? [];
+
+    const onSelect = (id: number) => {
+        if (status !== "none") return;
+
+        setSelectedOption(id);
+    };
 
     const title =
         challenge.type === "ASSIST"
@@ -62,9 +72,9 @@ export const Quiz = ({
                             )}
                             <Challenge
                                 options={options}
-                                onSelect={() => {}}
-                                status="none"
-                                selectedOption={undefined}
+                                onSelect={onSelect}
+                                status={status}
+                                selectedOption={selectedOption}
                                 disabled={false}
                                 type={challenge.type}
                             />
@@ -72,6 +82,11 @@ export const Quiz = ({
                     </div>
                 </div>
             </div>
+            <Footer
+                disabled={!selectedOption}
+                status={status}
+                onCheck={() => {}}
+            />
         </>
     );
 };
