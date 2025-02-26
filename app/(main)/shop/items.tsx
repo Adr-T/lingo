8bb22bb12532618b4eca8ef8aec406d8c0a30dab
Adrien <ad.tranchand@gmail.com>
2025-02-26 18:20:44 +0100

    shop/items.tsx update with subscription button

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -28,6 +28,10 @@ export const Items = ({ hearts, points, hasActiveSubscription }: Props) => {
         });
     };
 
+    const onUpgrade = () => {
+        startTransition(() => {});
+    };
+
     return (
         <ul className="w-full">
             <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
@@ -56,6 +60,25 @@ export const Items = ({ hearts, points, hasActiveSubscription }: Props) => {
                             <p>{POINTS_TO_REFILL}</p>
                         </div>
                     )}
+                </Button>
+            </div>
+            <div className="flex items-center w-full p-4 pt-8 gap-x-4 border-t-2">
+                <Image
+                    src="/unlimited.svg"
+                    alt="Unlimited"
+                    height={60}
+                    width={60}
+                />
+                <div className="flex-1">
+                    <p className="text-neutral-700 text-base lg:text-xl font-bold">
+                        Unlimited hearts
+                    </p>
+                </div>
+                <Button
+                    onClick={onUpgrade}
+                    disabled={pending || hasActiveSubscription}
+                >
+                    {hasActiveSubscription ? "active" : "upgrade"}
                 </Button>
             </div>
         </ul>
